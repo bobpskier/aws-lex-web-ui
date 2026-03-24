78da4a4ef755a41645324e70fcf3eefa4a955569
atjohns <atjohns2@gmail.com>
2026-03-24 08:43:57 -0400

    Migrate to smithy and upgrade aws-sdk to latest

--- lex-web-ui/src/store/sigv4-handlers.js
+++ lex-web-ui/src/store/sigv4-handlers.js
@@ -17,7 +17,7 @@
  * This module provides request signing functionality using AWS SDK v3's SignatureV4.
  */
 
-import { SignatureV4 } from '@aws-sdk/signature-v4';
+import { SignatureV4 } from '@smithy/signature-v4';
 import { Sha256 } from '@aws-crypto/sha256-js';
 import { HttpRequest } from '@smithy/protocol-http';
 
